946562a16aa223716b3d6b7fe7675e03279fa71d
Thiago Benega <thiagobenega@gmail.com>
2025-01-31 10:52:03 -0300

    Implement reservations module

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,5 +1,5 @@
 import { Schema } from '@nestjs/mongoose';
-import { FilterQuery, Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 import { AbstractDocument } from './abstract.schema';
 import { Logger, NotFoundException } from '@nestjs/common';
 
@@ -36,7 +36,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
 
   async findOneAndUpdate(
     filterQuery: FilterQuery<TDocument>,
-    update: Partial<TDocument>,
+    update: UpdateQuery<TDocument>,
   ): Promise<TDocument> {
     const document = await this.model
       .findOneAndUpdate(filterQuery, update, { new: true })
